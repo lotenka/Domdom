183f5c78cbc223356670e16143a6c10c1366150f
lotenka <makmartynyuk@gmail.com>
2026-04-21 18:39:15 +0300

    Added function of control temperature at home(AC + boiler)

--- main/main.c
+++ main/main.c
@@ -11,15 +11,20 @@ static const char *TAG = "example";
 
 #define DHT_TYPE DHT_TYPE_AM2301
 #define DHT_DATA_GPIO 11
-
+#define RELAY_PIN 5   // GPIO для реле
+#define AC_PIN 6
 
 int ledPin = 10;
 int sensorPin = 9;
 volatile int state = 0;   // ОБЩАЯ переменная. Меняется в vRequeest
 volatile int ledState = 0;
+float g_temperature = 0;  //Температура
+
 
 void setup()
 {
+    gpio_set_direction(RELAY_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(RELAY_PIN, 0); // котёл выкл
     gpio_set_direction(ledPin, GPIO_MODE_OUTPUT);
     gpio_set_direction(sensorPin, GPIO_MODE_INPUT);
 }
@@ -31,6 +36,7 @@ void vDHT_read(void *pvParameters)
     {
         if (dht_read_float_data(DHT_TYPE, DHT_DATA_GPIO, &humidity, &temperature) == ESP_OK)
         {
+            g_temperature = temperature;
             printf("Humidity: %.1f%% Temp: %.1fC\n", humidity, temperature);
         }
         else
@@ -82,6 +88,38 @@ void vLight(void *pvParameters)
     }
 }
 
+
+//Управление котлом и кондиционером
+void vClimate(void *pvParameters)
+{
+    while (1)
+    {
+        if (g_temperature > 25.0)
+        {
+            // включить кондиционер
+            gpio_set_level(AC_PIN, 1);
+            gpio_set_level(RELAY_PIN, 0);
+            printf("AC ON\n");
+        }
+        else if (g_temperature < 22.0)
+        {
+            // включить котёл
+            gpio_set_level(RELAY_PIN, 1);
+            gpio_set_level(AC_PIN, 0);
+            printf("HEATER ON\n");
+        }
+        else
+        {
+            // всё выключено
+            gpio_set_level(RELAY_PIN, 0);
+            gpio_set_level(AC_PIN, 0);
+            printf("IDLE\n");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
+
 void app_main(void)
 {
     setup();
@@ -89,4 +127,5 @@ void app_main(void)
     xTaskCreate(vRequest, "Request", 2048, NULL, 2, NULL);
     xTaskCreate(vLight,   "Light",   2048, NULL, 1, NULL);
     xTaskCreate(vDHT_read, "DHT_read", configMINIMAL_STACK_SIZE * 3, NULL, 2, NULL);
+    xTaskCreate(vClimate, "Climate", 2048, NULL, 2, NULL);
 }
