99dff92156d4d9a7d7820b585e10a6d28e54877f
lotenka <makmartynyuk@gmail.com>
2026-04-22 16:00:56 +0300

    WIFI connection implemented. Successful build, firmware not tested. MQTT code comeneted.

--- main/main.c
+++ main/main.c
@@ -5,22 +5,85 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "dht.h"
+//#include "mqtt_client.h"
+
+#include "wifi_manager.h"
+
+
 
 static const char *TAG = "example";
 
-
 #define DHT_TYPE DHT_TYPE_AM2301
 #define DHT_DATA_GPIO 11
-#define RELAY_PIN 5   // GPIO для реле
+
+#define RELAY_PIN 5             // GPIO для реле
 #define AC_PIN 6
+
+//#define MQTT_BROKER "mqtt://broker.hivemq.com"
+
+
 
 int ledPin = 10;
 int sensorPin = 9;
-volatile int state = 0;   // ОБЩАЯ переменная. Меняется в vRequeest
+volatile int state = 0;         // ОБЩАЯ переменная. Меняется в vRequeest
 volatile int ledState = 0;
-float g_temperature = 0;  //Температура
+float g_temperature = 0;        //Температура
 
 
+/*
+static esp_mqtt_client_handle_t client;
+
+void mqtt_event_handler(void *handler_args, esp_event_base_t base,
+                        int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI("MQTT", "Connected");
+            esp_mqtt_client_subscribe(client, "home/boiler/set", 0);
+            esp_mqtt_client_subscribe(client, "home/ac/set", 0);
+            break;
+
+        case MQTT_EVENT_DATA:
+        {
+            char topic[64];
+            char data[64];
+
+            snprintf(topic, event->topic_len + 1, "%s", event->topic);
+            snprintf(data, event->data_len + 1, "%s", event->data);
+
+            ESP_LOGI("MQTT", "Topic: %s Data: %s", topic, data);
+
+            if (strcmp(topic, "home/boiler/set") == 0)
+            {
+                gpio_set_level(RELAY_PIN, atoi(data));
+            }
+            else if (strcmp(topic, "home/ac/set") == 0)
+            {
+                gpio_set_level(AC_PIN, atoi(data));
+            }
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = MQTT_BROKER,
+    };
+
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+*/
 void setup()
 {
     gpio_set_direction(RELAY_PIN, GPIO_MODE_OUTPUT);
@@ -37,6 +100,18 @@ void vDHT_read(void *pvParameters)
         if (dht_read_float_data(DHT_TYPE, DHT_DATA_GPIO, &humidity, &temperature) == ESP_OK)
         {
             g_temperature = temperature;
+
+            /*
+            //Публикация данных mqtt
+            char msg[50];
+            snprintf(msg, sizeof(msg), "%.1f", temperature);
+            esp_mqtt_client_publish(client, "home/temperature", msg, 0, 1, 0);
+
+            snprintf(msg, sizeof(msg), "%.1f", humidity);
+            esp_mqtt_client_publish(client, "home/humidity", msg, 0, 1, 0);
+            */
+
+
             printf("Humidity: %.1f%% Temp: %.1fC\n", humidity, temperature);
         }
         else
@@ -60,6 +135,13 @@ void vRequest(void *pvParameters)
         {
             vTaskDelay(pdMS_TO_TICKS(10));
         }
+
+        /*
+        //Публикация данных mqtt
+        char motion_msg[10];
+        snprintf(motion_msg, sizeof(motion_msg), "%d", state);
+        esp_mqtt_client_publish(client, "home/motion", motion_msg, 0, 1, 0);
+        */
     }
 }
 
@@ -123,7 +205,8 @@ void vClimate(void *pvParameters)
 void app_main(void)
 {
     setup();
-
+    //mqtt_app_start();
+    wifi_init();
     xTaskCreate(vRequest, "Request", 2048, NULL, 2, NULL);
     xTaskCreate(vLight,   "Light",   2048, NULL, 1, NULL);
     xTaskCreate(vDHT_read, "DHT_read", configMINIMAL_STACK_SIZE * 3, NULL, 2, NULL);
